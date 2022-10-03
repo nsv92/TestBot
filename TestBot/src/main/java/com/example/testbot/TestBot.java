f795b73c7b8add2c7e889d2bf672181963acc2dc
Sergei <svnat92@gmail.com>
2022-10-03 12:10:32 +0300

    Реализовал логирование

--- TestBot/src/main/java/com/example/testbot/TestBot.java
+++ TestBot/src/main/java/com/example/testbot/TestBot.java
@@ -1,14 +1,19 @@
 package com.example.testbot;
 
 import com.example.testbot.config.BotConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class TestBot extends TelegramLongPollingBot {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestBot.class);
 
     private final BotConfig config;
 
@@ -28,6 +33,8 @@ public class TestBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        String username = getUserNameFromUpdate(update);
+        LOGGER.info("New update received from: {}, message: {}",username, update.getMessage().getText());
         if (update.getMessage() != null) {
             Long chatId = update.getMessage().getChatId();
             String echo = update.getMessage().getText();
@@ -38,8 +45,16 @@ public class TestBot extends TelegramLongPollingBot {
             try {
                 execute(sendMessage);
             } catch (TelegramApiException e) {
+                LOGGER.error("Exception during sending message: {}", e.getMessage());
                 throw new RuntimeException(e);
             }
         }
     }
+
+    public String getUserNameFromUpdate(Update update) {
+        User user = update.getMessage().getFrom();
+        return (user.getUserName() == null ? user.getFirstName() : user.getUserName());
+    }
+
+
 }
--- TestBot/src/main/java/com/example/testbot/config/BotInit.java
+++ TestBot/src/main/java/com/example/testbot/config/BotInit.java
@@ -1,6 +1,8 @@
 package com.example.testbot.config;
 
 import com.example.testbot.TestBot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -12,6 +14,8 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 @Component
 public class BotInit {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestBot.class);
+
     @Autowired
     private TestBot bot;
 
@@ -20,8 +24,10 @@ public class BotInit {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         try {
             telegramBotsApi.registerBot(bot);
+            LOGGER.info("Bot registration -> success!");
         }
         catch (TelegramApiException e) {
+            LOGGER.error("Exception during bot registration: {}", e.getMessage());
             throw  new RuntimeException(e);
         }
     }
